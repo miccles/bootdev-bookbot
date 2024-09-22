a6b26d3b995c56ce362f6c757cd13b872a69fe35
miccles <michaeldamoulakis@gmail.com>
2024-09-22 12:11:29 -0400

    code modifications - added report func

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 def main():
     path_to_file = 'books/frankenstein.txt'
     text = get_text(path_to_file)
-    # print(text)
     num_words = count_words(text)
-    print(f'Number of words found: {num_words}')
-    letters = count_characters(text)
-    print(letters)
+    chars = count_characters(text)
+    make_report(path_to_file, num_words, chars_dict_to_sorted_list(chars))
 
 
 def get_text(path):
@@ -19,14 +17,35 @@ def count_words(text):
 
 
 def count_characters(text):
-    letter_count = {}
+    char_count = {}
     for char in text:
         char_low = char.lower()
-        if char_low in letter_count:
-            letter_count[char_low] +=1
+        if char_low in char_count:
+            char_count[char_low] +=1
         else:
-            letter_count[char_low] = 1
-    return letter_count
+            char_count[char_low] = 1
+    return char_count
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(chars):
+    chars_list = [{'char': char, 'num': chars[char]} for char in chars]
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
+
+
+def make_report(path, num_words, chars_list):
+    print(f'--- Begin report of {path} ---')
+    print(f'Words found in the document: {num_words}')
+    print('')
+    for char in chars_list:
+        character = char['char']
+        if character.isalpha():
+            print(f"Character '{character}' was found {char['num']} times")
+    print('--- End report ---')
 
 
 main()
