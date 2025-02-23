d76d036bff38dc3356ab979fad85d4d8af297841
miccles <michaeldamoulakis@gmail.com>
2025-02-23 14:59:17 -0500

    Made course modifications

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, chars_dict_to_sorted_list
 import sys
 
 def main():
@@ -13,41 +13,33 @@ def main():
 
 
 def get_text(path):
-    with open(path) as f:
+    with open(path, 'r', encoding='utf-8') as f:
         return f.read()
-    
 
+    
 
 
 def count_characters(text):
     char_count = {}
     for char in text:
         char_low = char.lower()
-        if char_low in char_count:
-            char_count[char_low] +=1
-        else:
-            char_count[char_low] = 1
+        if char_low.isalpha():  # Only count letters
+            if char_low in char_count:
+                char_count[char_low] += 1
+            else:
+                char_count[char_low] = 1
     return char_count
 
-
-def sort_on(d):
-    return d["num"]
-
-
-def chars_dict_to_sorted_list(chars):
-    chars_list = [{'char': char, 'num': chars[char]} for char in chars]
-    chars_list.sort(reverse=True, key=sort_on)
-    return chars_list
 
 
 def make_report(path, num_words, chars_list):
     print(f'--- Begin report of {path} ---')
-    print(f'{num_words} words found in the document')
+    print(f'Found {num_words} total words')
     print('')
     for char in chars_list:
         character = char['char']
         if character.isalpha():
-            print(f"Character '{character}' was found {char['num']} times")
+            print(f"{character}: {char['num']}")
     print('--- End report ---')
 
 
--- stats.py
+++ stats.py
@@ -1,6 +1,13 @@
 
-
+def sort_on(d):
+    return d["num"]
 
 def count_words(text):
     text_split = text.split()
     return len(text_split)
+
+
+def chars_dict_to_sorted_list(chars):
+    chars_list = [{'char': char, 'num': chars[char]} for char in chars]
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
